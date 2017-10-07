7e6cb6bf5711e7a6af7d3e301b9c17322579d1a7
975545486@qq.com <975545486@qq.com>
2017-10-07 11:21:28 +0800

    修改.gitignore 文件

---
+++ bsweb-service-impl-flows/src/main/java/com/lj/app/core/common/flows/service/FlowExpenseTestServiceImpl.java
@@ -0,0 +1,10 @@
+package com.lj.app.core.common.flows.service;
+
+import com.lj.app.core.common.base.service.BaseServiceImpl;
+
+import org.springframework.stereotype.Service;
+
+@Service("flowExpenseTestService")
+public class FlowExpenseTestServiceImpl<FlowExpenseTest> extends BaseServiceImpl<FlowExpenseTest> implements FlowExpenseTestService<FlowExpenseTest>{
+
+}
---
+++ bsweb-service-impl-flows/src/main/java/com/lj/app/core/common/flows/service/FlowUpmUserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.lj.app.core.common.flows.service;
+
+import com.lj.app.core.common.base.service.BaseServiceImpl;
+
+import org.springframework.stereotype.Service;
+
+@Service("flowUpmUserService")
+public class FlowUpmUserServiceImpl<FlowUpmUser> extends BaseServiceImpl<FlowUpmUser> implements FlowUpmUserService<FlowUpmUser>{
+
+}
