15d9ff72f39e9ef4b4f3d10337903726db6076eb
975545486@qq.com <975545486@qq.com>
2017-11-11 13:44:00 +0800

    通知管理编辑页面修改

--- bsweb-app-upm/src/main/java/com/lj/app/bsweb/upm/upmNotice/web/UpmNoticeAction.java
+++ bsweb-app-upm/src/main/java/com/lj/app/bsweb/upm/upmNotice/web/UpmNoticeAction.java
@@ -1,5 +1,7 @@
 package com.lj.app.bsweb.upm.upmNotice.web;
 
+import java.util.Date;
+
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.Result;
@@ -8,9 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.lj.app.bsweb.upm.AbstractBaseUpmAction;
+import com.lj.app.core.common.base.entity.BaseEntity;
 import com.lj.app.core.common.base.service.BaseService;
 import com.lj.app.core.common.notify.entity.UpmNotice;
 import com.lj.app.core.common.notify.service.UpmNoticeService;
+import com.lj.app.core.common.util.DateUtil;
+import com.lj.app.core.common.util.StringUtil;
 import com.lj.app.core.common.web.AbstractBaseAction;
 import com.opensymphony.xwork2.util.logging.Logger;
 import com.opensymphony.xwork2.util.logging.LoggerFactory;
@@ -55,6 +60,41 @@ public class UpmNoticeAction extends AbstractBaseUpmAction<UpmNotice> {
 			upmNotice = (UpmNotice)upmNoticeService.getInfoByKey(upmNotice.getId());
 		} else {
 			upmNotice = new UpmNotice();
+		}
+	}
+	
+	/**
+	 * 公共保存或者更新方法
+	 * @return
+	 * @throws Exception
+	 */
+	public String commonSaveOrUpdate() throws Exception {
+		
+	try{
+			if (StringUtil.isEqualsIgnoreCase(operate, AbstractBaseAction.EDIT)) {
+				BaseEntity entity = (BaseEntity)getModel();
+				entity.setUpdateBy(this.getLoginUserId());
+				entity.setUpdateByUname(this.getUserName());
+				entity.setUpdateDate(DateUtil.getNowDateYYYYMMddHHMMSS());
+				
+				getBaseService().updateObject(entity);
+				returnMessage = UPDATE_SUCCESS;
+			}else{
+				BaseEntity entity = (BaseEntity)getModel();
+				entity.setCreateBy(this.getLoginUserId());
+				entity.setCreateByUname(this.getUserName());
+				entity.setCreateDate(DateUtil.getNowDateYYYYMMddHHMMSS());
+				
+				upmNotice.setCreateDateTime(new Date());
+				getBaseService().insertObject(entity);
+				returnMessage = CREATE_SUCCESS;
+			}
+			
+			return LIST;
+		}catch(Exception e){
+			returnMessage = CREATE_FAILURE;
+			e.printStackTrace();
+			throw e;
 		}
 	}
 
